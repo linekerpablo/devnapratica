d0260cc2dcd99f2a03c3e5dc2cd8055adfb93ade
Lineker Silva <linekerpablo@gmail.com>
2025-10-21 16:10:37 -0300

    feat: Primeiro commit DevNaPratica

---
+++ Controllers/StatusController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevNaPraticaApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetStatus()
+        {
+            return Ok(new { message = "API funcionando!" });
+        }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using DevNaPraticaApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevNaPraticaApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private static readonly List<User> users = new();
+        private static int nextId = 1;
+
+        [HttpPost]
+        public IActionResult CreateUser([FromBody]User user)
+        {
+            if (string.IsNullOrWhiteSpace(user?.Name))
+                return BadRequest(new { message = "Nome de usuario e obrigatorio!" });
+
+            user.Id = nextId++;
+            users.Add(user);
+
+            return CreatedAtAction(
+                nameof(GetUserById),
+                new { id = user.Id },
+                user
+                );
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetUserById(int id)
+        {
+            var user = users.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+                return NotFound(new { message = "Usuario nao encontrado!" });
+            return Ok(user);
+        }
+    }
+}
